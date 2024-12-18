ea672c05cfd62831951e96e67d7a8070be79e7b1
junoprice <jonas.3636@hotmail.com>
2024-12-18 08:45:56 -0300

    :sparkles: Criação do Menu

---
+++ Menu.ts
@@ -0,0 +1,97 @@
+import readlinesync = require("readline-sync");
+import { colors } from './src/util/Colors';
+
+export function main() {
+
+    let opcao: number;
+
+    while (true) {
+
+        console.log(colors.bg.black, colors.fg.yellow, 
+                    "*****************************************************");
+        console.log("                                                     ");
+        console.log("                 FARMACIA BEM ESTAR                  ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Produto                        ");
+        console.log("            2 - Listar todos os produtos             ");
+        console.log("            3 - Buscar Produto por ID                ");
+        console.log("            4 - Atualizar Dados do Produto           ");
+        console.log("            5 - Apagar Produto                       ");
+        console.log("            6 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ", 
+        colors.reset);
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao == 9) {
+            console.log(colors.fg.greenstrong, 
+                "\nFarmacia Bem Estar - Remedio barato é aqui!");
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nCriar  Produto\n\n", colors.reset);
+                
+                keyPress()
+                break;
+            case 2:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nListar todas as  Produto\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 3:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nConsultar dados da  Produto - por número\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 4:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nAtualizar dados da Produto\n\n", colors.reset);
+
+                keyPress()
+                break;
+            case 5:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nApagar uma  Produto\n\n", colors.reset);
+
+                keyPress()
+                break;
+
+            default:
+                console.log(colors.fg.whitestrong, 
+                    "\nOpção Inválida!\n", colors.reset);
+
+                keyPress()
+                break;
+        }
+    }
+
+}
+
+/* Função com os dados da pessoa desenvolvedora */
+function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Generation Brasil - generation@generation.org");
+    console.log("github.com/conteudoGeneration");
+    console.log("*****************************************************");
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+main();
